6bdac436963dbb0af348561d2324fece541bf6d9
NickSan123 <jnnicolai123@gmail.com>
2021-10-15 16:51:22 -0300

    Casa

--- Back/src/ProEventos.API/data/DataContex.cs
+++ Back/src/ProEventos.API/data/DataContex.cs
@@ -3,7 +3,7 @@ using ProEventos.API.Models;
 
 namespace ProEventos.API.data
 {
-    public class DataContex:DbContext
+    public class DataContex : DbContext
     {
         public DataContex(DbContextOptions<DataContex> options) : base(options){}
         public DbSet<Evento> Eventos { get; set; }
